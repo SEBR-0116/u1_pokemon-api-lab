9386ef5a525cb48b22efa35d2879ff763cfdcd39
connorreidy1 <connorjreidy@gmail.com>
2024-01-23 14:49:25 -0700

    HTML,JS after lab session finished

---
+++ script.js
@@ -0,0 +1,35 @@
+// const getPokemon = async () => {
+//     const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+//     console.log(pokemon.data.results)
+// }
+// let button = document.querySelector('#searchButton')
+// button.addEventListener('click', async (event) =>{
+//     event.preventDefault
+//     let pokemonName = document.querySelector("#pokemonName")
+//     let pokemonImage = document.querySelector("#pokemonImage")
+//     let textInput = document.querySelector("#inputBar")
+//     let pokemon = textInput.value
+//     let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/squirtle/`)
+//     console.log(response.data.name)
+//     pokemonName.innerHTML= `${pokemon}`
+    
+
+//     let pokemonImage1=response.data.sprites.other.home.front_default
+//     pokemonImage.src = pokemonImage1
+
+ 
+
+//   console.log(pokemonImage1)
+
+
+//     console.log(response)
+// })
+
+
+const button = document.querySelector('#searchButton')
+const inputBar = document.querySelector('#inputBar')
+
+const getPokemon = async () => {
+    const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${inputBar}`)
+    console.log(response) 
+}
