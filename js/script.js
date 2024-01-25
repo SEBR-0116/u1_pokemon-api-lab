0ccebbc6df1d42cd9bb3c12b3cd9cc368fde051b
Denesh Anandathasan <denesh555@gmail.com>
2024-01-25 14:14:01 -0500

    Part 1 : partially completed

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let button2 = document.querySelector("#searchButton2")
 button.addEventListener('click', async () => {
 
     let pokemonName = document.querySelector("#pokemonName")
+    let pokeSprites=document.querySelector('#poke-sprites')
     let pokemonImage1 = document.querySelector("#pokemonImage1")
     let pokemonImage2 = document.querySelector("#pokemonImage2")
     let pokemonImage3 = document.querySelector("#pokemonImage3")
@@ -14,6 +15,8 @@ button.addEventListener('click', async () => {
     let ability = document.querySelector("#abilities")
     let pokemonTypes = document.querySelector('types') 
     let abilityNames= document.querySelector('.row1')
+    let baseExperience= document.querySelector('.row2')
+    let pokeMoves= document.querySelector('.row3')
     let pokeTypes= document.querySelector('.row2')
 
 
@@ -21,19 +24,13 @@ button.addEventListener('click', async () => {
     let textInput = document.querySelector("#inputBar").value
     
     let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
-    console.log(response.data)
-    //console.log(response.data.sprites)
-    console.log(response.data.types)
-    //console.log(response.data.name)
+    //console.log(response.data)
+  
     let pName =response.data.name
-    //console.log(response.data)  
-    //let spritesDefault= response.data.sprites.front_default
-    //let spritesShiny= response.data.sprites.back_shiny
-    //let pPics =response.data.sprites
+
     // let pAbilities = response.data.abilities[0].ability.name
     let pAbilities = response.data.abilities
-    //console.log(pAbilities)
-    // let abilityList =[]
+
 
     let PokeAbility = []
     for(const element of pAbilities) {
@@ -43,30 +40,45 @@ button.addEventListener('click', async () => {
 
    
     //console.log(PokeTypes)
-    let pokeTypeslist = loopArratys(response.data.types,type.name).toString()
-    console.log(pokeTypeslist)
+    // let pokeTypeslist = loopArratys(response.data.types,type.name).toString()
+    // console.log(pokeTypeslist)
 
-    pokemonName.innerHTML =`<h4> <span class="text">Name :<span> ${pName}</h4>`
+//     let pokeTypeslist=[]
+//     for(const element of arraysList) {
+//     //console.log(element.ability.name)
+//     listofArray.push(`element.${root}`)
+// }
+
+/////// Display Zone
+
+    pokemonName.innerHTML =`<h4> Name  :: ${pName}</h4>`
+    //pokeSprites.innerHTML =`<p> <span class="text"> Sprites <span> </p>`
     pokemonImage1.innerHTML=`<img src=${response.data.sprites.front_default}>`
     pokemonImage2.innerHTML =`<img src=${response.data.sprites.back_shiny}>`
     pokemonImage3.innerHTML =`<img src=${response.data.sprites.front_default}>`
     pokemonImage4.innerHTML =`<img src=${response.data.sprites.front_shiny}>`
-    abilityNames.innerHTML = `<span class="text">Ability :<span> ${PokeAbility.toString()}`
-    //pokeTypes.innerHTML =`<span class="test">Types :<span> ${loopArratys(response.data.types,type.name).toString} <span>`
+    abilityNames.innerHTML = ` <p> Abilities :: ${PokeAbility.toString()}</p>`
+    baseExperience.innerHTML = `<p> Base Experience :: ${response.data.abilities.base_experience}</p>`
+    let movesList=[response.data.moves[0]]
+    //console.log(response.data.moves[0][1])
+    //console.log(movesList)
     
+    // for(let i=0;i<movesList.length;i++){
+    //    console.log(movesList[0][i].name)
+    // }
 
 }
 )
 
 
-function loopArratys(arraysList,root){
-let listofArray=[]
-for(const element of arraysList) {
-    //console.log(element.ability.name)
-    listofArray.push(`element.${root}`)
-}
-return listofArray
-}
+// function loopArratys(arraysList,root){
+// let listofArray=[]
+// for(const element of arraysList) {
+//     //console.log(element.ability.name)
+//     listofArray.push(`element.${root}`)
+// }
+// return listofArray
+// }
 
 
 button2.addEventListener('click',async ()=>{
@@ -77,7 +89,7 @@ let mainDiv = document.getElementById('.main');
 let paragraph = document.createElement('p');
 
 let textInput = document.querySelector("#inputBar2").value
-let response = await axios.get(`https://pokeapi.co/api/v2/type/${textInput}`)
+let response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${textInput}`)
 
 console.log(response)
 
