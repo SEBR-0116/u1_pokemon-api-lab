ff5208f1df010a6545679ab57be7b46e3fcf9754
MiaDHayes <miadeyon@outlook.com>
2024-01-26 07:18:55 -0600

    moved types above image

--- script.js
+++ script.js
@@ -17,17 +17,25 @@ button.addEventListener('click', async (event) => {
     const pokemonName = document.querySelector('#pokemonName')
     const pokemonImage = document.querySelector('#pokemonImage')
     const pokemonStats = document.querySelector('#pokemonStats')
+    const pokemonTypes = document.querySelector('#pokemonTypes')
     let textInput = inputBar.value
     console.log(textInput)
     let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
     pokemonName.innerText = response.data.name
     pokemonImage.src = response.data.sprites.front_default
     pokemonStats.innerHTML= ''
+    pokemonTypes.innerText = ''
 
     response.data.stats.forEach(stat => {
         const statElement = document.createElement('p')
         statElement.textContent = `${stat.stat.name}: ${stat.base_stat}`
         pokemonStats.appendChild(statElement)
+    })
+    
+    response.data.types.forEach(type => {
+        const typeElement = document.createElement('p')
+        typeElement.textContent = type.type.name
+        pokemonTypes.appendChild(typeElement)
     })
 })
 
